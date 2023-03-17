324e41a7a8dbd20dce4706cc72b1eea6203823d3
XorysDev <xorysteam.dev@gmail.com>
2023-03-17 09:58:34 +0600

    up files

--- blogApi/urls.py
+++ blogApi/urls.py
@@ -16,11 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="test api for blog api py.26",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/accounts/', include('account.urls')),
     path('api/v1/categories/', include('category.urls')),
     path('api/v1/posts/', include('post.urls')),
@@ -30,14 +49,13 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# TODO comments
 # TODO ViewSET, actions
 
 # TODO likes
 # TODO favorites
 
 # TODO Images
-# TODO documentation, filer, search, pagination
+# TODO filer, search, pagination
 
 # TODO deploy
 # TODO followings
